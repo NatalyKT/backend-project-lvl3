6a4bc1de480593f95385af78e35be52cf94c3582
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-12-30 00:25:15 +0300

    modified:  bin/page_loader.js

--- bin/page_loader.js
+++ bin/page_loader.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import fs from 'fs';
+import path from 'path';
 import pageLoader from '../src/loadPage.js';
-
-/* тред из StackOverflow на тему:
-https://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code
-Возможно использование в кач-ве получ-я номера версии из package.json:
 
 const installingPack = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8');
 const { version } = JSON.parse(installingPack);
-*/
 
 program
-  .version('1.0.0')
-  // или: .version(version) - если вар-т выше, с installingPack
+  .version(version)
   .description('Download page and save it to file system')
   .arguments('<url>')
   .option('-o, --output [path]', 'output dir path', process.cwd())
@@ -21,13 +17,10 @@ program
     pageLoader(url, program.output);
   })
   .action((url) => pageLoader(url, program.opts().output)
-    .then((path) => console.log(`\nOpen ${path}`))
+    .then((filePath) => console.log(`\nOpen ${filePath}`))
     .catch((err) => {
       console.error(err);
       process.exit(1);
-    }))
-// .parse(process.argv);
-// eslint-disable-next-line semi-style
-;
+    }));
 
 program.parse(process.argv);
