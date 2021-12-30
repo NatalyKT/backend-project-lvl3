83d8da7f862f768de737df876872539e41537289
Natalia Tyshchenko <nataly_kt@hotmail.com>
2021-12-30 12:36:01 +0300

    add: determining version number from package.json

--- bin/page_loader.js
+++ bin/page_loader.js
@@ -3,7 +3,11 @@
 import program from 'commander';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import pageLoader from '../src/loadPage.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const installingPack = fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8');
 const { version } = JSON.parse(installingPack);
